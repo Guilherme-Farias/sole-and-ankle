db92476bdf758f5799eec9429e9f528ec77215c5
Guilherme Farias <guicfarias11@gmail.com>
2021-12-18 17:48:03 -0300

    SuperHeader finished

--- src/components/SuperHeader/SuperHeader.js
+++ src/components/SuperHeader/SuperHeader.js
@@ -23,19 +23,27 @@ const SuperHeader = () => {
 };
 
 const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
   font-size: 0.875rem;
   color: ${COLORS.gray[300]};
   background-color: ${COLORS.gray[900]};
-`;
+  height: 40px;
+  padding-left: 32px;
+  padding-right: 32px;
+  `;
 
 const MarketingMessage = styled.span`
   color: ${COLORS.white};
-`;
+  margin-right:auto;
+  `;
 
 const HelpLink = styled.a`
   color: inherit;
   text-decoration: none;
   outline-offset: 2px;
+  margin-left: 32px;
+  margin-right: 32px;
 
   &:not(:focus-visible) {
     outline: none;
